d56db6fc80c1f56764932d2711778acb4867a7cc
田中彩美紗 <amisasm@tanakaayamishanoMacBook-Air.local>
2024-01-21 22:34:42 +0900

    test 1/21

--- src/Hellow.java
+++ src/Hellow.java
@@ -3,8 +3,8 @@ public class Hellow {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("例外が"
-				 "発生しました。");
+		System.out.println("例外が");
+		System.out.println("test");
 
 	}
 
